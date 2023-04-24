cece59da860d3c825ba48e0cf29292e778f9a51a
daniel.falzon <danie.falzon6@gmail.com>
2023-04-25 00:21:02 +0200

    Lesson 13: Get assignments by API

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -16,11 +16,17 @@ export default {
     data(){
         return{
             assignments: [
-                { id:1, name: 'Finish project 1', complete: false, tag: 'math' },
-                { id: 2, name: 'Finish project 2', complete: false, tag: 'science' },
-                { id: 3, name: 'Finish project 3', complete: false, tag: 'math' },
+
             ]
         }
+    },
+
+    created(){
+        fetch('http://localhost:3001/assignments')
+            .then(response => response.json())
+            .then(assignments => {
+                this.assignments = assignments;
+            })
     },
 
     computed: {
